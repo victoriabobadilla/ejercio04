55dd938122b217289d69d9a14737e1afb298a1cc
alumno <alumno@LIMSFLAB06-15D.system-uap.adm>
2017-11-27 19:44:08 -0500

    first commit

---
+++ src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio04;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("9.5 X 4.5 - 2.5 X 3");
+		System.out.println("-------------------");
+		System.out.println("     45.5 - 3.5");
+		System.out.println("         =");
+		System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+    }
+    
+}
